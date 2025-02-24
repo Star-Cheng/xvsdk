a98a68a7347ace11fa97c1a460c5c014c2e39756
star-cheng <1335929010@qq.com>
2025-02-24 18:08:52 +0800

    更新sdk代码

--- study/open_rgb/open_rgb.cpp
+++ study/open_rgb/open_rgb.cpp
@@ -2,10 +2,38 @@
 #include <xv-sdk.h> // Stereo PRO SDK 头文件
 #include "fps_count.hpp"
 #include <opencv2/opencv.hpp> // OpenCV 头文件，用于图像处理和显示
+#include <cstdlib>
+#include <thread>
+#include <fstream>
+#include <sstream>
+#include <cmath>
+#include <mutex>
+#include <signal.h>
+#include <cstring>
 
 // 回调函数，用于处理 RGB 图像数据
 void rgbCallback(const xv::ColorImage &rgb)
+// void rgbCallback(const xv::ColorImage &im)
+
 {
+    //////////////////////////////////////////////////////////////////
+    // auto rgb = im.toRgb();
+    // std::shared_ptr<std::uint8_t> data(new std::uint8_t[rgb.width*rgb.height], std::default_delete<std::uint8_t[]>());
+    // for (std::size_t i=0; i < rgb.width*rgb.height; ++i) {
+    //     data.get()[i] = 0.299*rgb.data.get()[3*i]+0.587*rgb.data.get()[3*i+1]+0.114*rgb.data.get()[3*i+2];
+    // }
+    // xv::GrayScaleImage img;
+    // img.width = rgb.width;
+    // img.height = rgb.height;
+    // img.data = data;
+    // // s_rgb_tags = rgbDetector.detect(img);
+    // // s_rgb_gray = img;
+    // // 将 RGB 图像数据转换为 OpenCV 的 Mat 格式
+    // // cv::Mat img = raw_to_opencv(rgb);
+    // cv::Mat image(rgb.height, rgb.width, CV_8UC1, const_cast<uint8_t *>(rgb.data.get()));
+    // cv::imshow("RGB Camera", img);
+    // cv::waitKey(1); // 等待 1ms，用于刷新图像显示
+    //////////////////////////////////////////////////////////////////
     static int frameCount = 0;
     static FpsCount fc;
     fc.tic();
@@ -16,6 +44,7 @@ void rgbCallback(const xv::ColorImage &rgb)
     cv::Mat image(rgb.height, rgb.width, CV_8UC1, const_cast<uint8_t *>(rgb.data.get()));
     cv::imshow("RGB Camera", image);
     cv::waitKey(1); // 等待 1ms，用于刷新图像显示
+    //////////////////////////////////////////////////////////////////
 }
 int main(int argc, char *argv[])
 {
